5ae9c00d72cc6c987e0c62e94f7ed64c2cf7e5e5
Evan Kalvis <evankalvis@hotmail.com>
2017-01-15 06:24:23 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+ $('a[href^="#"]').on('click', function (e){
+   e.preventDefault();
+   var target=this.hash;
+   var $target=$(target);
+   
+   $('html, body').animate({
+     'scrollTop':$target.offset().top
+   }, 1300, 'swing', function () {
+     window.location.hash=target;
+   });
+ });
+});
